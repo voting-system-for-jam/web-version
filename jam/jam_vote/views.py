a78fe5a166522404c4d556ddb5dc79fc284eeef0
AigaGoto <s1811337@u.tsukuba.ac.jp>
2021-02-26 16:07:47 +0900

    投票結果を表示できるようにしました

--- jam/jam_vote/views.py
+++ jam/jam_vote/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import get_object_or_404, redirect, render
 from django.views.generic import TemplateView, CreateView, DetailView, UpdateView, DeleteView, ListView
 from django.urls import reverse_lazy
 
 from .models import Question, Team
 from .forms import QuestionForm, TeamForm, PostCreateFormSet
+from django.http.response import HttpResponseRedirect
+from django.urls.base import reverse
 
 # Create your views here.
 
@@ -17,40 +19,25 @@ class Index(TemplateView):
             'question_list':question_list,
         }
         return context
-    # model(データベース)から色々引っ張ってくる
-    # def get_context_data(self, *args, **kwargs):
-    #     context = super().get_context_data(**kwargs)
-    #     post_list = Post.objects.all().order_by('-created_at')
-    #     context = {
-    #         'post_list': post_list,
-    #     }
-    #     return context
 
-
-# class MakeView(CreateView):
-#     template_name = 'jam_vote/make.html'
-#     model = Question
-#     form_class = VoteForm
-#     success_url = reverse_lazy('jam_vote:index')
 
 def add(request):
     question = QuestionForm(request.POST or None)
     context = {'question': question}
     if request.method == 'POST' and question.is_valid():
         post = question.save(commit=False)
-        formset = PostCreateFormSet(request.POST, instance=post)  # 今回はファイルなのでrequest.FILESが必要
+        formset = PostCreateFormSet(request.POST, instance=post) 
+        print(formset)
         if formset.is_valid():
             post.save()
             formset.save()
             return redirect('jam_vote:index')
 
-        # エラーメッセージつきのformsetをテンプレートへ渡すため、contextに格納
         else:
+            print("OK")
             context['formset'] = formset
 
-    # GETのとき
     else:
-        # 空のformsetをテンプレートへ渡す
         context['formset'] = PostCreateFormSet()
 
 
@@ -69,3 +56,34 @@ class AnswerView(DetailView):
             'team_list': team_list,
         }
         return context
+
+
+class AnswerEndView(TemplateView):
+    template_name = 'jam_vote/answer_end.html'
+
+class ResultsView(DetailView):
+    model = Question
+    template_name = 'jam_vote/result.html'
+
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        your_team_choice = question.team_set.get(pk=request.POST['your_team'])
+        favorite_team_choice = question.team_set.get(pk=request.POST['favorite_team'])
+        best_team_choice = question.team_set.get(pk=request.POST['best_team'])
+
+    except (KeyError, Team.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'jam_vote/index.html', {
+            'question': question,
+            'error_message': "選択していない選択肢があります",
+        })
+    else:
+        your_team_choice.yourTeamCount += 1
+        your_team_choice.save(update_fields=['yourTeamCount'])
+        favorite_team_choice.favoriteTeamCount += 1
+        favorite_team_choice.save(update_fields=['favoriteTeamCount'])
+        best_team_choice.bestTeamCount += 1
+        best_team_choice.save(update_fields=['bestTeamCount'])
+        return HttpResponseRedirect(reverse('jam_vote:answer_end'))
