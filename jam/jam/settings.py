01d5c8920634bfb5e82ac0cfe0a7316b96bf5728
AigaGoto <s1811337@u.tsukuba.ac.jp>
2021-03-02 16:49:10 +0900

    見た目を変更しました。

--- jam/jam/settings.py
+++ jam/jam/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -124,4 +125,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 # dockerだとcssが適用されていなかったので、以下一行を追加
-STATIC_ROOT = '/static'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = '/usr/src/jam/static'
+
