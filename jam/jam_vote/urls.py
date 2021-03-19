adbc6ab049ac18cf38fdc10d9243d603beb00e84
AigaGoto <s1811337@u.tsukuba.ac.jp>
2021-03-19 14:14:45 +0900

    投票の削除機能を追加しました

--- jam/jam_vote/urls.py
+++ jam/jam_vote/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('make', views.add, name='make'),
     path('answer/<int:pk>', views.AnswerView.as_view(), name='answer'),
     path('result/<int:pk>', views.ResultsView.as_view(), name='result'),
+    path('delete/<int:pk>', views.DelView.as_view(), name='delete'),
     path('answer_end', views.AnswerEndView.as_view(), name='answer_end'),
     path('<int:question_id>/plot', views.img_plot, name='img_plot'),
     path('<int:question_id>/vote', views.vote, name='vote'),
